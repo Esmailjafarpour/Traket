3ef68d125eb4b631e91778fee0f32997b3b50124
javad1365 <34204851+javad1365@users.noreply.github.com>
2024-06-16 19:12:56 +0330

    get data user

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,19 @@
-import {BrowserRouter} from "react-router-dom";
-import Router from "router/Router"
-
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { BrowserRouter } from "react-router-dom";
+import defaultOptions from "configs/reactQuery";
+import Router from "router/Router";
 
 function App() {
-  return <div>
-   <BrowserRouter>
-     <Router/>
-   </BrowserRouter>
-  </div>
+  const queryClient = new QueryClient({defaultOptions});
+  return (
+    <QueryClientProvider client={queryClient}>
+        <BrowserRouter>
+          <Router />
+        </BrowserRouter>
+      <ReactQueryDevtools/>
+    </QueryClientProvider>
+  );
 }
 
 export default App;
--- src/router/router.jsx
+++ src/router/router.jsx
@@ -1,11 +1,18 @@
+import { useQuery } from "@tanstack/react-query";
 import { Routes, Route } from "react-router-dom";
 import HomePage from "pages/HomePage";
 import AdminPage from "pages/AdminPage";
 import DashboardPage from "pages/DashboardPage";
 import AuthPage from "pages/AuthPage";
 import PageNotFound from "pages/404";
+import { getProfile } from "services/user";
 
 const Router = () => {
+  const {data , isLoading , error} = useQuery(["profile"],getProfile)
+  console.log({data,isLoading,error});
+  if (isLoading) return <h1>Loading</h1>
+    
+  
   return (
     <Routes>
       <Route index element={<HomePage />} />
